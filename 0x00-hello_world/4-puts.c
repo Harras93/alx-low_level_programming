dca8bf2dee983e110a904254041f0708f8504398
Harras93 <mahoudharras990@gmail.com>
2023-06-15 10:52:51 -0700

    4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,11 +1,10 @@
 #iclude < stdio.h >
 /**
  *main - A Programming is like building with puts function
-Return:0 (Success)
+*Return: 0 (Success)
 */
-int main(void)
-{
-	puts("\"porgraming is like building a multilingual puzzle");
+int main(void) 
+{puts("\"porgraming is like building a multilingual puzzle");
 return (0);
 }
 
